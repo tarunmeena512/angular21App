5023a3fceb10c402e3ae630af8274b9905707614
Tarun Meena <tarunmeena512@gmail.com>
2026-04-01 12:59:33 +0530

    adding dashboard store

--- angular21/src/app/components/dashboard-page/dashboard-page.ts
+++ angular21/src/app/components/dashboard-page/dashboard-page.ts
@@ -1,9 +1,8 @@
 import { ChangeDetectionStrategy, Component, computed, inject, signal } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { HttpClient } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 import { MatTableModule } from '@angular/material/table';
-import { rxResource, takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
 import { Subject } from 'rxjs';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -12,7 +11,7 @@ import { User } from '../../models';
 import { UsersStore } from '../../stores/dashboard.store';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-dashboard-page',
@@ -24,6 +23,8 @@ import { FormsModule } from '@angular/forms';
     MatIconModule,
     FormsModule,
     MatButtonModule,
+    ReactiveFormsModule,
+    FormsModule,
     MatInputModule,
   ],
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -32,44 +33,19 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './dashboard-page.scss',
 })
 export class DashboardPage {
-  private route = inject(ActivatedRoute);
-  private usersStore = inject(UsersStore);
+  displayedColumns: string[] = ['id', 'name', 'email', 'phone'];
 
-  private searchSubject = new Subject<string>();
+  route = inject(ActivatedRoute);
+  usersStore = inject(UsersStore);
+
   username = signal('');
   email = signal('');
-  searchQuery = signal('');
-
-  usersResource = this.usersStore.usersResource();
-  isLoading = () => this.usersStore.isLoading();
-  error = () => this.usersStore.error();
-  reload = () => this.usersStore.reload();
-
-  filteredUsers = computed(() => {
-    const query = this.searchQuery().toLowerCase().trim();
-    const allUsers = this.usersResource.value() ?? [];
-
-    return query ? allUsers.filter((u) => u.name.toLowerCase().includes(query)) : allUsers;
-  });
-
-  displayedColumns: string[] = ['id', 'name', 'email', 'phone'];
 
   constructor() {
     this.route.queryParams.pipe(takeUntilDestroyed()).subscribe((params) => {
       this.username.set(params['username'] || 'User');
       this.email.set(params['email'] || '');
     });
-
-    this.searchSubject
-      .pipe(debounceTime(300), distinctUntilChanged(), takeUntilDestroyed())
-      .subscribe((value) => {
-        this.searchQuery.set(value);
-      });
-  }
-
-  onSearch(event: Event) {
-    const value = (event.target as HTMLInputElement).value;
-    this.searchSubject.next(value);
   }
 
   trackByUserId(index: number, user: User): number {
--- angular21/src/app/stores/dashboard.store.ts
+++ angular21/src/app/stores/dashboard.store.ts
@@ -1,31 +1,64 @@
-import { inject } from '@angular/core';
-import { signalStore, withMethods, withState } from '@ngrx/signals';
+import { computed, inject } from '@angular/core';
+import { patchState, signalStore, withComputed, withMethods, withState } from '@ngrx/signals';
 import { rxResource } from '@angular/core/rxjs-interop';
 import { UserService } from '../services/users.service';
 import { User } from '../models';
+import { debounceTime, distinctUntilChanged, of, pipe, switchMap, tap } from 'rxjs';
+import { rxMethod } from '@ngrx/signals/rxjs-interop';
 
 export const UsersStore = signalStore(
-  withMethods(() => {
+  { providedIn: 'root' },
+
+  withState({
+    searchQuery: '',
+  }),
+
+  withMethods((store) => {
     const userService = inject(UserService);
 
     const usersResource = rxResource({
       stream: () => userService.getUsers(),
     });
 
+    const debouncedUpdate = rxMethod<string>(
+      pipe(
+        debounceTime(300),
+        distinctUntilChanged(),
+        switchMap((query) => {
+          patchState(store, { searchQuery: query });
+          return of(query);
+        }),
+      ),
+    );
     return {
-      usersResource() {
-        return usersResource;
-      },
-      isLoading() {
-        return usersResource.isLoading();
-      },
+      usersResource: () => usersResource,
 
-      error() {
-        return usersResource.error();
-      },
+      isLoading: computed(() => usersResource.isLoading()),
+      error: computed(() => usersResource.error()),
       reload() {
         usersResource.reload();
       },
+
+      onSearch(query: string) {
+        debouncedUpdate(query);
+      },
     };
   }),
+
+  withComputed((store) => ({
+    filteredUsers: computed(() => {
+      const resource = store.usersResource();
+      const allUsers = resource.value() ?? [];
+      const term = store.searchQuery().toLowerCase().trim();
+
+      if (!term) return allUsers;
+
+      return allUsers.filter(
+        (user: User) =>
+          user.name.toLowerCase().includes(term) ||
+          user.phone.toLowerCase().includes(term) ||
+          user.email.toLowerCase().includes(term),
+      );
+    }),
+  })),
 );
